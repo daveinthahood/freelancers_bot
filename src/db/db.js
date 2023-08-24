f87c657ac46a6b2988e67d48b9f57538b0f464d2
daveinthahood <davidesimone96@outlook.it>
2023-08-24 22:11:49 +0200

    add icon

--- src/db/db.js
+++ src/db/db.js
@@ -6,11 +6,11 @@
 
 export function getItems() {
     return [
-        {title: "HTML", price: 50.00, id:1 },
-        {title: "CSS", price: 50.00, id:2 },
-        {title: "JavaScript", price: 100.00, id:3},
-        {title: "React", price: 150.00, id:4},
-        {title: "Node", price: 150.00, id:5 },
-        {title: "Mongo", price: 70.00, id:6 }
+        {title: "HTML", price: 50.00, id:1, url_icon:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" },
+        {title: "CSS", price: 50.00, id:2, url_icon:"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" },
+        {title: "JavaScript", price: 100.00, id:3, url_icon:"https://raw.githubusercontent.com/simple-icons/simple-icons/11a1907124284b52a39c5db4cc1bb075a49c4108/icons/javascript.svg"},
+        {title: "React", price: 150.00, id:4, url_icon:"https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"},
+        {title: "Node", price: 150.00, id:5, url_icon:"https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" },
+        {title: "Mongo", price: 70.00, id:6, url_icon:"https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" }
     ]
 }
